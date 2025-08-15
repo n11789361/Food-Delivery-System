35711021e4d0f755144fa3216ed7d36944c53ca0
VAISHNAVI <n11789361@qut.edu.au>
2025-08-16 07:38:20 +1000

    backend file is added

---
+++ backend/controllers/taskcontroller.js
@@ -0,0 +1,11 @@
+const Task = require('../models/Task');
+const getTasks = async (
+req,
+res) => {
+try {
+const tasks = await Task.find({ userId: req.user.id });
+res.json(tasks);
+} catch (error) {
+res.status(500).json({ message: error.message });
+}
+};
